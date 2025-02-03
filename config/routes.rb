84ec9f0e67de81dfc5b69ae4fd04ed6cfdc26592
KoheiYagi <192582422+koheiyagi1206@users.noreply.github.com>
2025-02-03 07:43:44 +0000

    [Add]added search function.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root :to =>"homes#top"
+
   get "home/about"=>"homes#about"
+  get '/search', to: 'searches#search'
 
   devise_for :users
 
